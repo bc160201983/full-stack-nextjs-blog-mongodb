b58f7e04f95201e6b33b7f00ba8169be73f42da4
Muhammad Bilal <41289168+bc160201983@users.noreply.github.com>
2022-12-24 11:46:15 +0000

    changes

--- pages/api/posts/getPosts.js
+++ pages/api/posts/getPosts.js
@@ -2,7 +2,20 @@ import connectdb from "../../../database/connect";
 
 const getPosts = async (req, res) => {
   await connectdb();
-  return <div>getPosts</div>;
+  const { method } = req;
+  if (method != "POST") {
+    return res
+      .status(500)
+      .json({ success: false, message: "only post method allowed" });
+  }
+
+  try {
+    res.status(200).json({ success: true, message: "User has been created" });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+    });
+  }
 };
 
 export default getPosts;
